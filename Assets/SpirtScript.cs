4267b9953e7cb42e18e934c90cf0009524c3af79
ladwan <ladwanhester@gmail.com>
2018-07-20 10:29:45 -0400

    Initial Push

---
+++ Assets/SpirtScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpirtScript : MonoBehaviour {
+
+    public Collider Checkbox;
+    public GameObject player;
+    bool go = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You very alone,  you want dance?");
+            StartCoroutine(Wait());
+        }
+    }
+  // Update is called once per frame
+  void Update ()
+    {
+        if (go == true)
+        {
+            transform.Translate(0, 0 , 2);
+            Debug.Log("Follow, Follow");
+
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        go = true;
+    }
+}
---
+++ Assets/playercontrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour {
+
+    // The target marker.
+    public Transform target;
+    
+    // Speed in units per sec.
+    public float speed;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Spirit")
+        {
+            Debug.Log("Sooo... Happy");
+            speed = 0;
+        }
+    }
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        // Move our position a step closer to the target.
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
