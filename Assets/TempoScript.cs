c8f0f5d551f25c499d5eb47cdfc2a2169b9114a9
ladwan <ladwanhester@gmail.com>
2018-08-16 16:38:26 -0400

    TempScript Updated

--- Assets/TempoScript.cs
+++ Assets/TempoScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TempoScript : MonoBehaviour {
 
@@ -9,10 +10,18 @@ public class TempoScript : MonoBehaviour {
     int BeatsPerMeasure;
      RawImage Vignette;
     Color alphacolor;
+    int CurrentDance = 1, CPUDance = 10,FailTick,ScoreTick;
+    public Text CompareText,FailText,ScoreText;
+    GameObject GameOverPanel;
+    bool GameisOver;
+    
    
 	// Use this for initialization
 	void Start ()
     {
+        GameOverPanel = GameObject.Find("GameOver");
+        GameOverPanel.SetActive(false);
+
         Vignette = TempoCanvas.transform.GetChild(0).GetComponent<RawImage>();
     
         StartCoroutine(Beat());
@@ -23,6 +32,42 @@ public class TempoScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if(GameisOver == true)
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+
+        FailText.text = FailTick.ToString();
+        ScoreText.text = ScoreTick.ToString();
+        if (FailTick >= 3)
+        {
+            GameOverPanel.SetActive(true);
+            GameisOver = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (CurrentDance == CPUDance)
+            {
+                CompareText.text = "Correct";
+                ScoreTick += 10 ;
+               // Debug.Log("Correct");
+            }
+            else
+            {
+                CompareText.text = "Fail";
+                FailTick++;
+                //Debug.Log("Fail");
+
+            }
+
+        }
+
+
         Vignette.color = alphacolor;
     }
 
@@ -31,12 +76,14 @@ public class TempoScript : MonoBehaviour {
         
 
         yield return new WaitForSecondsRealtime(0.5f);
+        CurrentDance = 1;
         BeatsPerMeasure++;
         alphacolor.a = 0.2f;
         Invoke("TurnOffVignette", 0.1f);
 
         if (BeatsPerMeasure >= 4)
         {
+            CurrentDance = 10;
             alphacolor.a = 0.75f;
             //Debug.Log("Beat");
             BeatsPerMeasure = 0;
